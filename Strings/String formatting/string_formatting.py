d2253fc5ff0b047c161362f8f96fb946b603100c
warlinx <58605680+warlinx@users.noreply.github.com>
2021-11-10 18:22:05 +0300

    change

--- Strings/String formatting/string_formatting.py
+++ Strings/String formatting/string_formatting.py
@@ -2,4 +2,4 @@ name = "John"
 # Note: %s is inside the string, % is after the string.
 print("Hello, PyCharm! My name is %s!" % name)
 
-print("I'm ??? years old" % ???)
+print("I'm %d years old" %17)
